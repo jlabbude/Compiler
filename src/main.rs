c96e4bf2c77c15c20a7f3777f51e781c25bf7c4b
Lucas <jlabbude@hotmail.com>
2024-10-04 14:31:14 -0300

    finish2

--- src/main.rs
+++ src/main.rs
@@ -50,8 +50,8 @@ fn main() {
                     | Token::Identifier(_)
                     | Token::Operator(_) => print!("{expression:?} "),
                     Token::Separator(separator) => match separator {
-                        Separator::NewLine => println!(),
-                        Separator::WhiteSpace => print!(" "),
+                        Separator::NewLine => print!("{}", Separator::NewLine.to_string()),
+                        Separator::WhiteSpace => print!("{}", Separator::WhiteSpace.to_string()),
                         _ => print!("{expression:?} "),
                     },
                 }
