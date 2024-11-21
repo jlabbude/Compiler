74c5bd71e7b588f861ad4e0ef55dbaefd9a4cf89
Lucas <jlabbude@hotmail.com>
2024-11-21 15:18:09 -0300

    remove csv

--- src/main.rs
+++ src/main.rs
@@ -27,29 +27,12 @@ fn main() {
     let source_file = Path::new(&args[1]);
     match check_file(source_file) {
         Ok(_) => {
-            let mut reserved_words = Vec::new();
-            let mut literals = Vec::new();
-            let mut identifiers = Vec::new();
-            let mut separators = Vec::new();
-            let mut comments = Vec::new();
-            let mut operators = Vec::new();
             let tokens = tokenize(
                 &std::fs::read_to_string(source_file)
                     .unwrap()
                     .replace("\r\n", "\n") // windows newline
             );
             tokens.iter().for_each(|expression| {
-                match expression {
-                    Token::ReservedWord(tokens) => reserved_words.push(tokens.to_string()),
-                    Token::Literal(tokens) => literals.push(tokens.to_string()),
-                    Token::Identifier(tokens) => identifiers.push(tokens.to_string()),
-                    Token::Comment(comment) => comments.push(comment.to_string()),
-                    Token::Separator(tokens) => match tokens {
-                        Separator::WhiteSpace | Separator::NewLine => (),
-                        _ => separators.push(tokens.to_string()),
-                    },
-                    Token::Operator(tokens) => operators.push(tokens.to_string()),
-                }
                 match expression {
                     Token::ReservedWord(_)
                     | Token::Literal(_)
@@ -63,46 +46,6 @@ fn main() {
                     },
                 }
             });
-            let max_length = *[
-                reserved_words.len(),
-                literals.len(),
-                identifiers.len(),
-                separators.len(),
-                operators.len(),
-                comments.len(),
-            ]
-            .iter()
-            .max()
-            .unwrap();
-            reserved_words.resize(max_length, String::new());
-            literals.resize(max_length, String::new());
-            identifiers.resize(max_length, String::new());
-            separators.resize(max_length, String::new());
-            operators.resize(max_length, String::new());
-            comments.resize(max_length, String::new());
-            let file = std::fs::File::create("output/output.csv").expect("Could not create file.");
-            let mut wtr = csv::Writer::from_writer(file);
-            wtr.write_record([
-                "Reserved Words",
-                "Literals",
-                "Identifiers",
-                "Separators",
-                "Operators",
-                "Comments",
-            ])
-            .unwrap();
-            for i in 0..max_length {
-                wtr.write_record([
-                    &reserved_words[i],
-                    &literals[i],
-                    &identifiers[i],
-                    &separators[i],
-                    &operators[i],
-                    &comments[i],
-                ])
-                .unwrap();
-            }
-            wtr.flush().unwrap();
             std::process::exit(0);
         }
         Err(e) => {
